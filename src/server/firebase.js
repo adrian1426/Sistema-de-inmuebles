5b1065a5e59e049b5eebcb1a59971fe3448fe9c1
Adrian1426 <adrianhh1426@gmail.com>
2020-09-03 23:38:12 -0500

    configurando firebase en react

---
+++ src/server/firebase.js
@@ -0,0 +1,23 @@
+import app from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyDV37wg-LKXXPwQHLQYkOUA0G1laup-rds",
+  authDomain: "home-5a1bd.firebaseapp.com",
+  databaseURL: "https://home-5a1bd.firebaseio.com",
+  projectId: "home-5a1bd",
+  storageBucket: "home-5a1bd.appspot.com",
+  messagingSenderId: "393268007986",
+  appId: "1:393268007986:web:432910810170abd3fa982b",
+  measurementId: "G-CCBJY3QJQR"
+};
+
+class Firebase {
+
+  constructor() {
+    app.initializeApp(config);
+    this.db = app.firestore();
+  }
+
+};
+
+export default Firebase;

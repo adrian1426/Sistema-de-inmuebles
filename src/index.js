b240d74d2e1616e50b79b338396c0dac626696b1
Adrian1426 <adrianhh1426@gmail.com>
2020-09-08 23:45:49 -0500

    configurando index para nuevo provider de sesion

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Firebase, { FirebaseContext } from './server';
+import { initialState } from './sesion/initialState';
+import { StateProvider } from './sesion/store';
+import sesionReducer from './sesion/reducers/sesionReducer';
 
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={new Firebase()}>
-      <App />
+      <StateProvider initialState={initialState} reducer={sesionReducer}>
+        <App />
+      </StateProvider>
     </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
